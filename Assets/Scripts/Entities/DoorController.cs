aef40eb8787fae2656a9e1f8cc7a6b0ef2aa087e
Charles Mark Colling <collingmark@gmail.com>
2020-10-28 21:20:33 -0400

    Added a Door Tile, and a door object with animations and scripting

---
+++ Assets/Scripts/Entities/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public bool open;
+    bool lastState;
+    Animator Animator;
+    BoxCollider boxCollider;
+    void Start()
+    {
+        open = false;
+        lastState = false;
+        Animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastState != open)
+        {
+            lastState = open;
+            Animator.SetBool("open", lastState);
+            boxCollider.enabled = !open;
+        }
+    }
+}
