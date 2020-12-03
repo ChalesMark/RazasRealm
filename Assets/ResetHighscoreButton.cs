a963e8d859248a1fa4b2e56b0bbc8032ba6b5a36
James <drouinjr@hotmail.com>
2020-12-03 06:22:21 -0500

    added highscore reset button to hub

---
+++ Assets/ResetHighscoreButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetHighscoreButton : MonoBehaviour, IInteractable
+{
+    bool autoInteractable = false;
+    bool IInteractable.AutoInteract { get => autoInteractable; set => autoInteractable = value; }
+
+    public string GetInteractText()
+    {
+        return "Press F to Reset Highest Wave";
+    }
+
+    public void Interact(InteractController interactController)
+    {
+        PlayerPrefs.SetInt("Highscore", 0);
+        Camera.main.transform.Find("Canvas").Find("Highscore").GetComponent<Text>().text = "Highest Wave: " + PlayerPrefs.GetInt("Highscore").ToString();
+    }
+}
